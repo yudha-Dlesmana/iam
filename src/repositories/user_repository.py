eeb0121818cd407f5947f2a54ed214391b689ef3
Yudha <yudha@gmail.com>
2026-04-18 15:30:50 +0700

    refactor(user): update code pattern

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -2,28 +2,45 @@ from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models.user import User
-from src.schemas.user_schema import UserRequest, UserUpdateRequest
+from src.schemas.user_schema import UserCreateRequest, UserUpdateRequest
 
 class UserRepository:
-    def __init__(self, db: AsyncSession):
+    def __init__(
+        self, db: AsyncSession
+    ):
         self.db = db
     
-    async def get_all_users(self) -> list[User]:
+
+    async def get_all_users(
+        self
+    ) -> list[User]:
         result = await self.db.execute(select(User))
 
         return list(result.scalars().all())
     
-    async def get_user_by_id(self, user_id: str) -> User | None:
+
+    async def get_user_by_id(
+        self, 
+        user_id: str
+    ) -> User | None:
         result = await self.db.get(User, user_id)
 
         return result
     
-    async def get_user_by_email(self, email: str) -> User | None:
+
+    async def get_user_by_email( 
+        self, 
+        email: str 
+    ) -> User | None:
         result = await self.db.execute(select(User).where(User.email == email))
 
         return result.scalar_one_or_none()
 
-    async def create_user(self, request: UserRequest) -> User:
+
+    async def create_user( 
+        self, 
+        request: UserCreateRequest 
+    ) -> User:
         new_user = User(
             email=request.email,
             password=request.password,
@@ -36,7 +53,12 @@ class UserRepository:
 
         return new_user
     
-    async def update_user(self, user_id: str, request: UserUpdateRequest) -> User | None:
+
+    async def update_user(
+        self, 
+        user_id: str, 
+        request: UserUpdateRequest
+    ) -> User | None:
         data = request.model_dump(exclude_none=True)
         stmt = update(User).where(User.id == user_id).values(**data)
         
@@ -45,7 +67,11 @@ class UserRepository:
         
         return await self.db.get(User, user_id)
 
-    async def delete_user(self, user_id: str) -> bool:
+
+    async def delete_user(
+        self, 
+        user_id: str
+    ) -> bool:
         user = await self.db.get(User, user_id)
         
         if not user:
@@ -55,6 +81,3 @@ class UserRepository:
         await self.db.commit()
 
         return True
-
-        
-
