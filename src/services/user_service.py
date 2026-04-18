6ddabe067c86787593d6a383dd463f9f8aff2ddd
Yudha <yudha@gmail.com>
2026-04-18 16:00:18 +0700

    refactor(user): update code and style service

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,118 +1,75 @@
 from fastapi import HTTPException
-from sqlalchemy.exc import IntegrityError, OperationalError
 
 from src.schemas.base_schema import BaseResponse
-from src.schemas.user_schema import UserResponse, UserRequest, UpdatePasswordRequest, UpdateRoleRequest
+from src.schemas.user_schema import UserResponse, UserCreateRequest, UserUpdateRequest
 from src.repositories.user_repository import UserRepository
 
 
 class UserService:
-    def __init__(self, repo: UserRepository):
+    def __init__(
+        self, 
+        repo: UserRepository
+    ):
         self.repo = repo
 
-    async def get_all_users(self) -> BaseResponse[list[UserResponse]]:
-        try:
-            users = await self.repo.get_all_users()
-            data = [UserResponse.model_validate(user) for user in users]
-
-        except OperationalError:
-            raise HTTPException(status_code=503, detail="Database unavailable")
-
-        return BaseResponse(
-                message="User fetched",
-                data=data
-            )
-
-    async def get_user_by_id(self, user_id: str) -> BaseResponse[UserResponse]:
-        try:
-            user = await self.repo.get_user_by_id(user_id)
-
-        except OperationalError:
-            raise HTTPException(status_code=503, detail="Database unavailable")
-
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-
-        return BaseResponse(
-            message="User fetched",
-            data=UserResponse.model_validate(user)
-        )
-    
-    async def get_user_by_email(self, user_email: str) -> BaseResponse[UserResponse]:
-        try:
-            user = await self.repo.get_user_by_email(user_email)
-
-        except OperationalError:
-            raise HTTPException(status_code=503, detail="Database unavailable")
-
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-
-        return BaseResponse(
-            message="User fetched",
-            data=UserResponse.model_validate(user)
-        )
-    
-    async def create_user(self, request: UserRequest) -> BaseResponse[UserResponse]:
-        try:
-            user = await self.repo.create_user(request)
+    async def get_all_users(
+        self
+    ) -> BaseResponse[list[UserResponse]]:
+        users = await self.repo.get_all_users()
+        data = [UserResponse.model_validate(user) for user in users]
         
-        except IntegrityError as e:
-            if e.orig and e.orig.args[0] == 1062:
-                raise HTTPException(status_code=409, detail=f"Duplicate value: {e.orig.args[1]}")
-            if e.orig and e.orig.args[0] == 1452:
-                raise HTTPException(status_code=404, detail=f"Referenced record not found: {e.orig.args[1]}")
-            raise HTTPException(status_code=409, detail=f"Conflict: {e.orig.args[1] if e.orig else str(e)}")
-        except OperationalError:
-            raise HTTPException(status_code=503, detail="Database unavailable")
+        return BaseResponse(message="User fetched", data=data)
 
-        return BaseResponse(
-            message="User created",
-            data=UserResponse.model_validate(user)
-        )
-
-    async def update_role_user(self, user_id: str, request: UpdateRoleRequest) -> BaseResponse[UserResponse]:
-        try:
-            user = await self.repo.update_user(user_id, request=request)
-
-        except IntegrityError as e:
-            raise HTTPException(status_code=404, detail=f"Referenced record not found: {e.orig.args[1]}")
-        except OperationalError:
-            raise HTTPException(status_code=503, detail="Database unavailable")
+    async def get_user_by_id(
+        self, 
+        user_id: str
+    ) -> BaseResponse[UserResponse]:
+        user = await self.repo.get_user_by_id(user_id)
+        data = UserResponse.model_validate(user)
         
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
         
-        return BaseResponse(
-            message="User updated",
-            data=UserResponse.model_validate(user)
-        )
-    
-    async def update_password_user(self, user_id: str, request: UpdatePasswordRequest) -> BaseResponse[UserResponse]:
-        try:
-            user = await self.repo.update_user(user_id, request=request)
+        return BaseResponse(message="User fetched", data=data)
 
-        except OperationalError:
-            raise HTTPException(status_code=503, detail="Database unavailable")
+    async def get_user_by_email(
+        self, 
+        user_email: str
+    ) -> BaseResponse[UserResponse]:
+        user = await self.repo.get_user_by_email(user_email)
+        data = UserResponse.model_validate(user)
         
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
         
-        return BaseResponse(
-            message="User updated",
-            data=UserResponse.model_validate(user)
-        )
-    
+        return BaseResponse(message="User fetched", data=data)
+
+    async def create_user(
+        self, 
+        request: UserCreateRequest
+    ) -> BaseResponse[UserResponse]:
+        user = await self.repo.create_user(request)
+        data=UserResponse.model_validate(user)
+        
+        return BaseResponse(message="User created", data=data)
+
+    async def update_user(
+        self, 
+        user_id: str, 
+        request: UserUpdateRequest
+    ) -> BaseResponse[UserResponse]:
+        user = await self.repo.update_user(user_id, request)
+        data=UserResponse.model_validate(user)
+
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return BaseResponse(message="User updated", data=data)
+
     async def delete_user(self, user_id: str) -> BaseResponse[None]:
-        try:
-            deleted = self.repo.delete_user(user_id)
+        deleted = await self.repo.delete_user(user_id)
 
-        except OperationalError:
-            raise HTTPException(status_code=503, detail="Database unavailable")
-        
         if not deleted:
             raise HTTPException(status_code=404, detail="User not found")
-        
-        return BaseResponse(
-            message="User deleted"
-        )
+            
+        return BaseResponse(message="User deleted")
