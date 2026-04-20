66d5ff3bc3a3a11f949e2e3e96d9441d8f12b289
Yudha <yudha@gmail.com>
2026-04-20 12:10:40 +0700

    refactor: improve readability of HealthRepository by adding line breaks to method signatures

--- src/repositories/health_repository.py
+++ src/repositories/health_repository.py
@@ -2,8 +2,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 
 class HealthRepository:
-    def __init__(self, db: AsyncSession):
+    def __init__(
+        self, 
+        db: AsyncSession
+    ):
         self.db = db
     
-    async def check_db(self) -> None:
+    
+    async def check_db(
+        self
+    ) -> None:
         await self.db.execute(text("SELECT 1"))
