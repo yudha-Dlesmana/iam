e23bfe49992d90cd6b2f41d85109ac5dc64d3008
Yudha <yudha@gmail.com>
2026-04-20 16:24:16 +0700

    feat: add JWT configuration and token generation utilities for authentication

--- src/core/config.py
+++ src/core/config.py
@@ -3,7 +3,14 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PORT: int = 8000
     HOST: str = "0.0.0.0"
+
     DB_URL: str
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES:int = 30
+    REFRESH_TOKEN_EXPIRES_DAYS:int = 7
+    
 
     class Config:
         env_file = ".env"
--- src/core/security.py
+++ src/core/security.py
@@ -1,13 +1,49 @@
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+
+from src.core.config import settings
+
 
 _ph = PasswordHasher()
 
 def hash_password(plain: str) -> str:
     return _ph.hash(plain)
 
+
 def verify_password(plain: str, hashed: str) -> bool:
     try:
         return _ph.verify(hashed, plain)
     except VerifyMismatchError:
         return False
+
+
+def create_access_token (subject: str) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {
+        "sub": subject, 
+        "exp": expire, 
+        "type": "access"
+    }
+
+    return jwt.encode(
+        payload, 
+        settings.JWT_SECRET_KEY, 
+        algorithm=settings.JWT_ALGORITHM
+    )
+
+
+def create_refresh_token(subject: str) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_EXPIRES_DAYS)
+    payload = {
+        "sub": subject,
+        "exp": expire,
+        "type": "refresh"
+    }
+
+    return jwt.encode(
+        payload,
+        settings.JWT_SECRET_KEY,
+        algorithm=settings.JWT_ALGORITHM
+    )
