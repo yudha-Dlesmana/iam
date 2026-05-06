2860d09d7a4ac251fec103e190cb5d23ffeb0b16
Yudha <yudha@gmail.com>
2026-05-06 15:33:04 +0700

    refactor: reorder router imports in api_router.py for consistency

--- src/routers/api_router.py
+++ src/routers/api_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
 from src.routers.health_router import router as health_router
-from src.routers.role_router import router as role_router
-from src.routers.user_router import router as user_router
 from src.routers.auth_router import router as auth_router
 from src.routers.oauth_router import router as oauth_router
+from src.routers.role_router import router as role_router
+from src.routers.user_router import router as user_router
 
 api_router = APIRouter(prefix="/api/v1")
 
