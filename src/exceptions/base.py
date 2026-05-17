f37433deb46ee5600d494d431ff83c4dbd5b69b0
Yudha <yudha@gmail.com>
2026-05-17 19:15:14 +0700

    feat: implement custom exception hierarchy and global FastAPI exception handler

---
+++ src/exceptions/base.py
@@ -0,0 +1,21 @@
+class AppException(Exception):
+    status_code: int = 500
+    message: str = "internal server error"
+
+    def __init__(self, message: str | None = None):
+        if message:
+            self.message = message
+        super().__init__(self.message)
+
+class NotFountError(AppException):
+    status_code = 404
+    message="resource not found"
+
+class ConflictError(AppException):
+    status_code = 409
+    message = "resource conflict"
+
+class ValidationError(AppException):
+    status_code = 422
+    message = "validation error"
+
