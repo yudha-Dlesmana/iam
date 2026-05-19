f42864cc17cdf6d4f44972747c11e787b4b26014
Yudha <yudha@gmail.com>
2026-05-19 15:54:53 +0700

    fix: commit database sessions

--- src/core/database.py
+++ src/core/database.py
@@ -20,6 +20,7 @@ async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         try:
             yield session
+            await session.commit()
         except Exception:
             await session.rollback()
             raise
