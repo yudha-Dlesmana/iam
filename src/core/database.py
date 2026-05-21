67efd4cd4a21e6f13ba431fbdf1c1457ccb7ac78
Yudha <yudha@gmail.com>
2026-05-21 13:21:49 +0700

    refactor: remove unused import of DeclarativeBase in database.py

--- src/core/database.py
+++ src/core/database.py
@@ -1,5 +1,4 @@
 from typing import AsyncGenerator
-from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
