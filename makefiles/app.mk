d5aabd43dc5ef6ef8625114742eca8b89202f11d
Yudha <yudha@gmail.com>
2026-05-23 14:46:21 +0700

    chore(makefile): add test, test-path, test-cov targets

--- makefiles/app.mk
+++ makefiles/app.mk
@@ -1,10 +1,17 @@
 PYTHON := .venv/bin/python3
 PIP := .venv/bin/pip
+PYTEST := .venv/bin/pytest
 
-.PHONY: install run
+.PHONY: install run test test-path test-cov
 
 install:
 	$(PIP) install -r requirements.txt
 
 run:
 	$(PYTHON) main.py
+
+test-path:
+	${PYTEST} $(path) -v
+
+test-cov:
+	${PYTEST} --cov=src --cov-report=term-missing
