10b2a536b9657ca9023543aed1258589957e021c
Yudha <yudha@gmail.com>
2026-05-23 14:48:47 +0700

    test: add role creation test for successful role posting

--- tests/roles/test_add.py
+++ tests/roles/test_add.py
@@ -1,3 +1,7 @@
+async def test_create_role(role):
+    assert (await role.post_role("admin")).status_code == 201
+
+
 async def test_create_duplicate_role_conflict(role):
     assert (await role.post_role("admin")).status_code == 201
     assert (await role.post_role("admin")).status_code == 409
