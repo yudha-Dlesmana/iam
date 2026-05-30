58cbb5cdb158c0763ea20da1bcde361be36664fa
Yudha <yudha@gmail.com>
2026-05-30 14:43:18 +0700

    chore(seeder): remove 'more' from service seeding list

--- scripts/seeder/service.py
+++ scripts/seeder/service.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models import Service
 
-SERVICE = ["iam", "more"]
+SERVICE = ["iam"]
 
 
 async def seed_service(session: AsyncSession) -> None:
