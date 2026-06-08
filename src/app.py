85dd7821776587731ecd8b34e845c38408f66a6d
yudha-Dlesmana <yudha.dlesmana@gmail.com>
2026-06-08 15:00:56 +0700

    feat(app): enhance FastAPI initialization with dynamic URL settings and add production service response

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi import FastAPI
 
+from src.core.config import settings
 from src.core.database import engine
 from src.core.logging import setup_logging
 from src.exceptions.handlers import register_exception_handlers
@@ -18,13 +19,21 @@ async def lifespan(app: FastAPI):
 
 setup_logging()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url=None if settings.is_production else "/docs",
+    redoc_url=None if settings.is_production else "/redoc",
+    openapi_url=None if settings.is_production else "/openapi.json",
+)
 register_exception_handlers(app)
 register_middlewares(app)
 
 
 @app.get("/", include_in_schema=False)
 def default():
+    if settings.is_production:
+        return JSONResponse({"service": "iam"})
+
     return RedirectResponse("/docs")
 
 
